7fff9fc9fa6d0d2ee633a2681c0236cf216a1dba
mustafa <devmustafasaeed@outlook.com>
2017-10-05 07:34:10 +0200

    minor ui tweaks

--- MedicalMarket/MedicalMarket/Controllers/HomeController.cs
+++ MedicalMarket/MedicalMarket/Controllers/HomeController.cs
@@ -5,14 +5,32 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using MedicalMarket.Models;
+using MedicalMarket.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace MedicalMarket.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public HomeController(ApplicationDbContext context)
+        {
+            this._context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var model = _context.Items.Include(i => i.Images).ToList();
+            return View(model);
+        }
+
+        public IActionResult Category(string id)
+        {
+            var model = _context.Items.Where(i => i.Category.Id == id)
+                                      .Include(i => i.Images)
+                                      .ToList();
+            return View("Index", model);
         }
 
         public IActionResult Preview()
---
+++ MedicalMarket/MedicalMarket/Extensions/StringExtension.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MedicalMarket.Extensions
+{
+    public static class StringExtension
+    {
+        public static string Truncate(this string value, int maxChars)
+        {
+            return value.Length <= maxChars ? value : value.Substring(0, maxChars) + "...";
+        }
+    }
+}
