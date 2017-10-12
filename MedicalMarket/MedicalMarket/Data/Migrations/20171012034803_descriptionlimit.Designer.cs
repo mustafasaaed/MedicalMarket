43aebaf5196a54f33039dce577c8f00c681667e5
mustafa <devmustafasaeed@outlook.com>
2017-10-12 05:56:55 +0200

    fixing logo url

--- MedicalMarket/MedicalMarket/Data/Migrations/20171012034803_descriptionlimit.Designer.cs
+++ MedicalMarket/MedicalMarket/Data/Migrations/20171012034803_descriptionlimit.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace MedicalMarket.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20171012034803_descriptionlimit")]
+    partial class descriptionlimit
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ MedicalMarket/MedicalMarket/Data/Migrations/20171012034803_descriptionlimit.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace MedicalMarket.Data.Migrations
+{
+    public partial class descriptionlimit : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Items",
+                type: "nvarchar(1000)",
+                maxLength: 1000,
+                nullable: false,
+                oldClrType: typeof(string));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Items",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(1000)",
+                oldMaxLength: 1000);
+        }
+    }
+}
